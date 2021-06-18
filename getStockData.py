ee8cdda7fc418bf129d679f477736a0569bde816
YiyiL <Yiyiluo1996@gmail.com>
2021-06-18 19:19:26 -0400

    Added !stock
    Get top 5 stocks live using api

---
+++ getStockData.py
@@ -0,0 +1,45 @@
+import config
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import json
+
+
+class getStockList:
+
+    def getJsonparsedData(url):
+        """
+        Receive the content of ``url``, parse it as JSON and return the object.
+
+        Parameters
+        ----------
+        url : str
+
+        Returns
+        -------
+        dict
+        """
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        return json.loads(data)
+
+    def mostGainerStock(self):
+        url = ("https://financialmodelingprep.com/api/v3/stock/gainers?apikey=%s" %
+               (config.FMP_API))
+        parsedData = (getStockList.getJsonparsedData(url))
+        mostGainerData = parsedData['mostGainerStock']
+        counter = 0
+        topFiveList = []
+        for i in mostGainerData:
+            while counter != 5:
+                topFiveList.append(str(counter+1) + ". " + mostGainerData[counter]['ticker'] + " $" + mostGainerData[counter]['price'] + " " +
+                                   mostGainerData[counter]['changesPercentage'])
+                counter += 1
+
+        # print("\n".join(topFiveList))
+        return topFiveList
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
 import config
+import getStockData
 
 client = discord.Client()
+stockData = getStockData.getStockList()
+topFiveStockList = stockData.mostGainerStock()
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("!stock"):
+        await message.channel.send("Top 5 Gainer Stocks Today: \n" + "\n".join(topFiveStockList))
 
 client.run(config.BOT_TOKEN)
